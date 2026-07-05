2b7715e6d6e1d59596b9563af4d33d3d480e05b9
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2026-07-05 21:21:47 +0100

    Add PostgreSQL and pgAdmin services to Docker Compose, implement JPA for Customer entity, and configure data source in application properties.

--- customer-service/src/main/java/com/danztee/customer/CustomerService.java
+++ customer-service/src/main/java/com/danztee/customer/CustomerService.java
@@ -3,8 +3,11 @@ package com.danztee.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void register(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder().firstName(request.firstName()).lastName(request.lastName()).email(request.email()).build();
+
+//        store customer in db
+        customerRepository.save(customer);
     }
 }
