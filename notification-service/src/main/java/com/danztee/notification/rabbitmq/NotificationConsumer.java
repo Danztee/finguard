66fc639575eab0db654942bf2e45facf0cd6eb13
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2026-07-06 18:28:19 +0100

    Implement NotificationConsumer to handle RabbitMQ notifications. Added consumer method to process NotificationRequest and integrated with NotificationService for sending notifications.

--- notification-service/src/main/java/com/danztee/notification/rabbitmq/NotificationConsumer.java
+++ notification-service/src/main/java/com/danztee/notification/rabbitmq/NotificationConsumer.java
@@ -1,4 +1,22 @@
 package com.danztee.notification.rabbitmq;
 
-public class NotifcationConsumer {
+import com.danztee.clients.fraud.NotificationRequest;
+import com.danztee.notification.NotificationService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class NotificationConsumer {
+    private final NotificationService notificationService;
+
+    @RabbitListener(queues = "${rabbitmq.queues.notification}")
+    public  void consumer(NotificationRequest notificationRequest) {
+        log.info("Received Notification Request: {}", notificationRequest);
+
+        notificationService.send(notificationRequest);
+    }
 }
