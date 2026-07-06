e98acbcb9faf3f5c7441cfdff0cd4205baa7a590
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2026-07-06 02:02:02 +0100

    Add notification service module to the project, including Maven configuration, application properties, entity definition, and main application class. Update pom.xml and IDE settings to include the new module. Adjust application.yml for notification service settings and add a custom banner.

--- notification-service/src/main/java/com/danztee/Notification.java
+++ notification-service/src/main/java/com/danztee/Notification.java
@@ -1,4 +1,20 @@
 package com.danztee;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Notification {
+
+    @Id
+    @SequenceGenerator(name = "notification_id_sequence", sequenceName = "notification_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "notification_id_sequence")
+    private Integer id;
 }
